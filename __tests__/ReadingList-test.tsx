68f75170dfc8cba23df145381dd034a7314d5fbc
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2025-04-27 21:08:40 +0530

    chore: add tests for ReadingList

---
+++ __tests__/ReadingList-test.tsx
@@ -0,0 +1,42 @@
+import ReadingList from "@/components/ReadingList";
+import { BPDataType } from "@/contexts/BPDataContext";
+import { ThemeProvider } from "@/theme/ThemeContext";
+import { render, within } from "@testing-library/react-native";
+import { ReactNode } from "react";
+
+describe("<ReadingList />", () => {
+  test("shows correct blood pressure and pulse rate inside each reading", () => {
+    const mockData: BPDataType[] = [
+      { id: "0", date: new Date(), bp_sys: 120, bp_dia: 80, pr: 70 },
+      { id: "1", date: new Date(), bp_sys: 130, bp_dia: 85, pr: 75 },
+    ];
+
+    const wrapper = ({ children }: { children: ReactNode }) => {
+      return <ThemeProvider>{children}</ThemeProvider>;
+    };
+
+    const { getAllByTestId } = render(<ReadingList data={mockData} />, {
+      wrapper,
+    });
+
+    const items = getAllByTestId("reading-item");
+
+    expect(items).toHaveLength(2);
+    expect(within(items[0]).getByText("120/80"));
+    expect(within(items[0]).getByText("70"));
+    expect(within(items[1]).getByText("130/85"));
+    expect(within(items[1]).getByText("75"));
+  });
+
+  test("renders correct text for empty list", () => {
+    const wrapper = ({ children }: { children: ReactNode }) => {
+      return <ThemeProvider>{children}</ThemeProvider>;
+    };
+
+    const { getByText } = render(<ReadingList data={[]} />, {
+      wrapper,
+    });
+
+    expect(getByText("No Readings Yet"));
+  });
+});
